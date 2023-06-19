bcfb187d6435dd76aca67c6e61656d2b4067edfe
Benny77777 <119400562+Benny77777@users.noreply.github.com>
2023-06-19 07:37:58 +0100

    Add files via upload

---
+++ MyFirstApp/src/main/kotlin/Convert Numeric Value To another Type.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var x: Int = 5
+    var z: Long =  x.toLong()
+    println(z)
+    var a = 123.35
+    var b: Int = a.toInt()
+    println(b)
+    var c = 6L//represents a long value
+    println(c)
+}
---
+++ MyFirstApp/src/main/kotlin/Hexadecimal and Binary Numbers.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+   var x = 0b1111
+    var y = 0xA
+    println(x)
+    println(y)
+}
